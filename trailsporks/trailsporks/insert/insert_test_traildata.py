b4f2c391b595464329c54e0a7d5b852220071d98
raphael <norlandrhagen@gmail.com>
2021-02-03 10:17:23 -0800

    connection to sqlite db, playing with inserting all test gpx

--- trailsporks/trailsporks/insert/insert_test_traildata.py
+++ trailsporks/trailsporks/insert/insert_test_traildata.py
@@ -3,11 +3,14 @@
 import pandas as pd
 import gpxpy
 import gpxpy.gpx
-
-
+import glob
+import os
 from trailsporks.insert import connection as cnxn
 
-trail_name = 'notg'
+
+def grab_trail_names_from_gpx_dir():
+    trail_names_list = pd.Series([os.path.basename(x) for x in glob.glob("../gpx/*")]).str.split(".",expand=True)[0].to_list()
+    return trail_names_list 
 
 def gpx_to_pandas(gpx_file_path):
     """Takes a gpx filepath, parses files with gpxpy into pandas dataframe of lat,lon,elev.
@@ -33,6 +36,12 @@ def gpx_to_pandas(gpx_file_path):
     gpx_df = pd.DataFrame({'lat':lat_list, 'lon':lon_list,'elev':elev_list})
     return gpx_df
 
+def grab_trail_ID(trail_name):
+    """returns trail ID for input trail_name"""
+    trail_id = cnxn.pandas_read_sql("""SELECT * FROM tblTrails WHERE trail_name = '{trail_name}'""".format(trail_name=trail_name)).reset_index()["index"][0]
+    return trail_id
+
+
 def build_trail_meta(trail_name):
     df = pd.DataFrame({'trail_name':["""{}""".format(trail_name)]})
     return df
@@ -42,9 +51,12 @@ def grab_trail_traj(trail_name):
     df = gpx_to_pandas(trail_traj_path)
     return df
 
-dftm = build_trail_meta(trail_name)
-dftraj = grab_trail_traj(trail_name)
-dftraj.insert(0,'trail_id',1)
+for trail in grab_trail_names_from_gpx_dir():
+    print(trail)
+    dftm = build_trail_meta(trail)
+    cnxn.pandas_to_postgres(dftm, 'tbltrails')
 
-cnxn.pandas_to_postgres(dftm, 'tbltrails')
-cnxn.pandas_to_postgres(dftraj, 'tbltrail_trajectory')
+    dftraj = grab_trail_traj(trail)
+    dftraj.insert(0,'trail_id',grab_trail_ID(trail))
+
+    cnxn.pandas_to_postgres(dftraj, 'tbltrail_trajectory')
