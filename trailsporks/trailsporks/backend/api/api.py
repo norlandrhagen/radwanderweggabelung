9884b2eb2f56ea0e5d10ce80365fb238425ed720
lambertsbennett <lambertsbennett@gmail.com>
2021-02-03 09:35:55 -0800

    Complete trail resource

--- trailsporks/trailsporks/backend/api/api.py
+++ trailsporks/trailsporks/backend/api/api.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = # URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
--- trailsporks/trailsporks/backend/api/resources/trail_res.py
+++ trailsporks/trailsporks/backend/api/resources/trail_res.py
@@ -13,13 +13,26 @@ class Trail(Resource):
         trail = TrailModel.get_by_name(name)
         if trail:
             return trail.json()
-        return {'message':'trail not found'}, 404
+        return {'message': 'trail not found'}, 404
         
     def post(self, name):
         if TrailModel.get_by_name(name):
             return {'message':'A trail with this name already exists.'}
-        # TBC 
+        
+        data = Trail.parser.parse_args()
+        trail = TrailModel(data)
+
+        try:
+            trail.upsert()
+        except:
+            return {'message': 'An error occurred uploading the trail.'}, 500
+
+        return trail.json(), 201 
 
     def delete(self, name):
-        # TBC
+        trail = TrailModel.get_by_name(name)
+        if trail:
+            trail.delete()
+        return {'message': 'Trail deleted.'} 
+
     
