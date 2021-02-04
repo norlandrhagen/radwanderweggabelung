e79538648cd0f99013c4cc5ccea720085eca8f1b
Bennett Lambert <lambertsbennett@gmail.com>
2021-02-04 09:11:07 -0800

    Update api.py
    Add import statements for resources.

--- trailsporks/trailsporks/backend/api/api.py
+++ trailsporks/trailsporks/backend/api/api.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_restful import Resource, Api
 from db import db
 
+from resources.trail_res import Trail
+from resources.trailmetadata_res import TrailMetadata
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = # URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
