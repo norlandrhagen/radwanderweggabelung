9b99816521b3e28bdc7874270c3fd92f425d8f64
lambertsbennett <lambertsbennett@gmail.com>
2021-02-04 13:11:55 -0800

    remove id from Trail Model

--- trailsporks/trailsporks/backend/api/models/trail.py
+++ trailsporks/trailsporks/backend/api/models/trail.py
@@ -6,7 +6,7 @@ class TrailModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
-    def __init__(self, id, name):
+    def __init__(self, name):
         self.name = name
 
     def json(self):
